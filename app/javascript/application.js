eb2acf1935b8a0736ba7eb041151a154f5e51f7e
Prativindya P <prativindya1310@gmail.com>
2022-11-01 15:23:35 +0530

    First commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,7 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
 import "./controllers"
 
 
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
+//= require bootstrap-sprocketsimport * as bootstrap from "bootstrap"
